69a862b8f8cb652341b225896403b941f99f379f
pablo-psichalis <pablo.psrz@gmail.com>
2020-12-13 21:12:01 +0100

    Search automatically after typing

--- src/routes/Main/Main.js
+++ src/routes/Main/Main.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getAlternatedMergedSearches } from '../../api/api'
 
 import { Result } from '../../components/Result'
@@ -13,24 +13,38 @@ const Main = () => {
   const handleSubmit = (evt) => {
     evt.preventDefault()
     if (searchText) {
-      getAlternatedMergedSearches(searchText).then(res => {
-        setResults(res)
-      })
+      doSearch(searchText)
     }
+  }
+
+  useEffect(() => {
+    let timeout = null
+    if (searchText) {
+      timeout = setTimeout(() => {
+        doSearch(searchText)
+      }, 300)
+    }
+    return () => timeout && clearTimeout(timeout)
+  }, [searchText])
+  
+  const doSearch = (searchText) => {
+    getAlternatedMergedSearches(searchText).then(res => {
+      setResults(res)
+    })
   }
 
   return (
     <div id="main">
       <header className="main-header">
         <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={searchText}
-            onChange={(e) => setSearchText(e.target.value)}
-          />
-          <button type="submit">
-            Buscar!
-          </button>
+          <div className="search-wrapper">
+            <input
+              className="search-input"
+              type="text"
+              value={searchText}
+              onChange={(e) => setSearchText(e.target.value)}
+            />
+          </div>
         </form>
       </header>
       <section id="results">
