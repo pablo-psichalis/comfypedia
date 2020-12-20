d8a7c572866d430171cf88add11bf6dc7399a819
pablo-psichalis <pablo.psrz@gmail.com>
2020-12-20 17:26:46 +0100

    Add Capacitor for Android app export

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Plugins } from '@capacitor/core';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Main from './Main/Main'
 
-const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route path="/" exact>
-        <Main />
-      </Route>
-    </Switch>
-  </BrowserRouter>
-)
+const useSplashHide = () => {
+  useEffect(() => {
+    Plugins.SplashScreen.hide();
+  }, []);
+}
+
+const App = () => {
+  useSplashHide()
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Main />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  )
+}
 
 export default App
